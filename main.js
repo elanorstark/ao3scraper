8b372e2417c27231e021cd655b35ee5866dd50c6
Elanor Stark <elanor99stark@gmail.com>
2021-11-25 20:08:10 +0000

    List fandoms and titles

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const listElement = document.getElementById("list");
+    const titleListElement = document.getElementById("title-list");
     const { titles } = await chrome.storage.local.get(['titles']);
     
     titles.forEach(title => {
         const li = document.createElement("li");
         li.innerText = title;
-        listElement.appendChild(li);
+        titleListElement.appendChild(li);
+    });
+
+    const fandomListElement = document.getElementById("fandom-list");
+    const { fandoms } = await chrome.storage.local.get(['fandoms']);
+    
+    fandoms.forEach(fandom => {
+        const li = document.createElement("li");
+        li.innerText = fandom;
+        fandomListElement.appendChild(li);
     });
 })
--- popup.js
+++ popup.js
@@ -4,24 +4,47 @@ async function getCurrentTab() {
     return tab;
 }
 
+async function allTitles(currentTabId) {
+    const results = await chrome.scripting.executeScript({
+        target: {
+            tabId: currentTabId,
+        },
+        func: () => {
+            return Array(...document.querySelectorAll(".index .blurb .header .heading:nth-child(1) a:nth-child(1)")).map(x => x.innerText);
+        }
+    });
+    return results;
+}
+
+async function allFandoms(currentTabId) {
+    const results = await chrome.scripting.executeScript({
+        target: {
+            tabId: currentTabId,
+        },
+        func: () => {
+            return Array(...document.querySelectorAll(".index .blurb .header .fandoms a")).map(x => x.innerText);
+        }
+    });
+    return results;
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const checkPageButton = document.getElementById('scrape');
     checkPageButton.addEventListener('click', async () => {
         const currentTab = await getCurrentTab();
         const currentTabId = currentTab.id;
 
-        const results = await chrome.scripting.executeScript({
-            target: {
-                tabId: currentTabId,
-            },
-            func: () => {
-                return Array(...document.querySelectorAll("h3 a")).map(x => x.innerText);
-            }
-        });
-
-        const titles = results[0].result;
+        titleResults = await allTitles(currentTabId);
+        const titles = titleResults[0].result;
         console.log("scrape", titles);
         chrome.storage.local.set({titles});
+
+        fandomResults = await allFandoms(currentTabId);
+        const fandoms = fandomResults[0].result;
+        console.log("scrape", fandoms);
+        chrome.storage.local.set({fandoms});
 
         await chrome.tabs.create({
              url: 'main.html'
